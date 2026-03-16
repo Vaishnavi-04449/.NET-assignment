7956a87173930c86acc3bce2444f708106a61f24
Vaishnavi-04449 <vaishnavias204@gmail.com>
2026-03-16 17:05:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,52 @@
-﻿class Program
+﻿
+
+class Student
+{
+    public string Name;
+    public int Age;
+    public int Marks;
+
+    public void GetGrade()
+    {
+        string grade = Marks switch
+        {
+            >= 90 => "A",
+            >= 75 => "B",
+            >= 50 => "C",
+            _ => "Fail"
+        };
+
+        Console.WriteLine("Grade: " + grade);
+    }
+}
+
+class Program
 {
     static void Main()
     {
-        int n, first = 0, second = 1, next;
-
-        Console.Write("Enter the number of series: ");
-        n = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Fibonacci Series is:");
+        Console.Write("Enter number of students: ");
+        int n = int.Parse(Console.ReadLine());
 
         for (int i = 1; i <= n; i++)
         {
-            Console.Write(first + " ");
+            Student s = new Student();
 
-            next = first + second;
-            first = second;
-            second = next;
+            Console.WriteLine("\nEnter details for Student " + i);
+
+            Console.Write("Enter Student Name: ");
+            s.Name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            s.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Marks: ");
+            s.Marks = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nStudent Name: " + s.Name);
+            Console.WriteLine("Age: " + s.Age);
+            Console.WriteLine("Marks: " + s.Marks);
+
+            s.GetGrade();
         }
     }
 }
