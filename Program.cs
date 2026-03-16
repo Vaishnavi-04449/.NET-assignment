d46e07cc84e143ca24f6a12e14beac8f2e01567e
Vaishnavi-04449 <vaishnavias204@gmail.com>
2026-03-16 17:04:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n, first = 0, second = 1, next;
+
+        Console.Write("Enter the number of series: ");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series is:");
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(first + " ");
+
+            next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
